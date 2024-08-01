8fbd0740837283790fcf7fbe56ddfdf866454b15
unknown <manshuchatrath20@gmail.com>
2024-08-01 18:28:51 -0400

    updated package.json

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const signUp_1 = require("./routes/signUp");
@@ -16,17 +18,18 @@ const cors = require("cors");
 app.use(cors());
 app.use(express_1.default.json());
 dotenv_1.default.config();
+console.log(process.env);
 app.use(signUp_1.signUpRouter);
 app.use(login_1.loginRouter);
 app.use(items_1.itemRouter);
 app.use(categories_1.categoryRouter);
 app.use(dishes_1.dishesRouter);
-console.log("hjere?");
+
 database_1.default
-    .sync()
-    .then((res) => {
+  .sync()
+  .then((res) => {
     app.listen(process.env.PORT || 4000, () => {
-        console.log(`Server is running on http://localhost:4000`);
+      console.log(`Server is running on http://localhost:4000`);
     });
-})
-    .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
