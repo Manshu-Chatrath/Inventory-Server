96995436e3b773d16ab21276b35d205cb5043df7
unknown <manshuchatrath20@gmail.com>
2024-08-11 22:37:13 -0400

    Added few logs

--- dist/index.js
+++ dist/index.js
@@ -25,6 +25,8 @@ app.use((0, express_session_1.default)({
     resave: false,
     saveUninitialized: false,
     cookie: {
+        sameSite: "none",
+        secure: true,
         maxAge: 1000 * 60 * 60,
     },
 }));
